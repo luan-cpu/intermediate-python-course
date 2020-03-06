6800ae9b9977d022d92766e5f0b2a68318e106b9
Lukas Gulliksen Andersen <lukas@Lukass-MacBook-Pro.local>
2020-03-06 19:11:46 +0100

    Now rolling two dice

--- dice_roller.py
+++ dice_roller.py
@@ -1,5 +1,13 @@
-def main():
-  print('You rolled a die')
+import random
+dice_rolls = 2
+for i in range(0,dice_rolls):
+  roll = random.randint(1,6)
+  print(f'You rolled a {roll}')
 
+def main():
+
+  roll = random.randint(1,6)
+  print(f'You rolled a {roll}')
+ 
 if __name__== "__main__":
   main()
